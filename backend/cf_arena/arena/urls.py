1124aceddd98798a11f582528130870943ae87d6
Mohit <mohit.balwani@peak.ai>
2021-03-27 14:55:43 +0530

    added get problems of user

--- backend/cf_arena/arena/urls.py
+++ backend/cf_arena/arena/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home),
     path("verify_user/", views.verify_user),
+    path('problems/', views.problems)
 ]
--- backend/cf_arena/arena/views.py
+++ backend/cf_arena/arena/views.py
@@ -23,10 +23,42 @@ def verify_user(request):
                     "rating": response["result"][0]["rating"],
                     "profile_pic": response["result"][0]["titlePhoto"],
                 }
-            )
+        ).json()
         else:
             return_payload = json.dumps(
                 {"status": "FAILED", "error": response["comment"]}
             )
         return HttpResponse(return_payload)
     return HttpResponse({"error": "Method not allowed"})
+    
+
+def problems(request):
+    if request.method == "GET":
+        data = request.GET
+        cf_handle = data.get("cf_handle")
+        return_payload = {}
+        response = requests.get(
+            f"https://codeforces.com/api/user.status?handle={cf_handle}"
+        ).json()
+        if response["status"] == "OK":
+            submissions = response.get('result')
+            problem_set = set([])
+            for problem in submissions:
+                contestId, index = problem["problem"].get("contestId",None), problem['problem']["index"]
+                if contestId and type(index)==str:
+                    print(contestId,index)
+                # problem_url = get_by_problem_url(contestId,index)
+
+                # print(contestId,index)
+                # problem_set.add(problem_url)
+
+            return_payload = json.dumps(
+                {
+                    "Problems": list(problem_set),
+                }
+            )
+        else:
+            return_payload = json.dumps({"error": response["comment"]})
+        return HttpResponse(return_payload)
+    return HttpResponse({"error": "Method not allowed"})
+    
