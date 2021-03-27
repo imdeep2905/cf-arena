49a9cb9c072130d376d86a0ea54e1a6afa7fd631
Mohit <mohit.balwani@peak.ai>
2021-03-27 19:33:46 +0530

    added match status api and write docstrings

--- backend/cf_arena/arena/helpers.py
+++ backend/cf_arena/arena/helpers.py
@@ -1,7 +1,12 @@
 import constants
-
+import requests
 
 def generate_problem_url(contest_id, problem_index):
     return (
         f"https://codeforces.com/contest/{contest_id}/problem/{problem_index}"
     )
+
+def helper_response(user):
+        return requests.get(
+            f"https://codeforces.com/api/user.status?handle={user}&count=3"
+        ).json()
