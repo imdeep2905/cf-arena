9a299f540b5c2530be204882cec1f04a14ff94e3
iamprayush <iamprayush@gmail.com>
2021-03-27 15:47:27 +0530

    Added models and API for problems fetched from CF.

--- backend/cf_arena/arena/urls.py
+++ backend/cf_arena/arena/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home),
-    path("verify_user/", views.verify_user),
+    path("all_problems_update/", views.AllProblemsUpdate.as_view(), name="all_problems_update"),
+    path("verify_user/", views.VerifyUser.as_view(), name="verify_user"),
 ]
--- backend/cf_arena/arena/views.py
+++ backend/cf_arena/arena/views.py
@@ -1,32 +1,84 @@
 import requests
-import json
-from django.http.response import HttpResponse
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
-def home(request):
-    return HttpResponse("Hello, world.")
+import constants
+from .models import *
 
 
-def verify_user(request):
-    if request.method == "GET":
+class AllProblemsUpdate(APIView):
+    def put(self, request):
+        '''
+        Updates the database with all problems from CodeForces.
+        '''
+        return_payload = {}
+        try:
+            response = requests.get(
+                f"{constants.CODEFORCES_URL}api/problemset.problems"
+            ).json()
+
+            if response['status'] != 'OK':
+                raise Exception(response["comment"])
+
+            for problem in response['result']['problems']:
+                contest_id = problem.get("contestId")
+                problem_index = problem.get("index")
+                rating = problem.get('rating')
+
+                if contest_id and problem_index and rating:
+                    problem_instance, created = AllProblems.objects.update_or_create(
+                        problem_url=f'https://codeforces.com/contest/{contest_id}/problem/{problem_index}',
+                        rating=rating,
+                    )
+
+            return_payload = {
+                "status": "OK",
+                "message": "Problems created/updated in database!"
+            }
+        except Exception as ex:
+            return_payload = {
+                "status": "FAILED",
+                "error": str(ex)
+            }
+
+        return Response(return_payload)
+
+
+class VerifyUser(APIView):
+    def get(self, request):
+        '''
+        Given a user handle, return user details if the user exists.
+
+        Input
+        =====
+        cf_handle (str): Handle of the user.
+
+        Output
+        ======
+        JsonResponse:
+            payload (dict):
+                - status (OK/FAILED)
+                - rating (int)
+                - profile_pic_url (str) 
+        '''
         data = request.GET
         cf_handle = data.get("cf_handle")
         return_payload = {}
-        response = requests.get(
-            f"https://codeforces.com/api/user.info?handles={cf_handle}"
-        ).json()
-        if response["status"] == "OK":
-            return_payload = json.dumps(
-                {
-                    "status": "OK",
-                    "rating": response["result"][0]["rating"],
-                    "profile_pic": response["result"][0]["titlePhoto"],
-                }
-            )
-        else:
-            return_payload = json.dumps(
-                {"status": "FAILED", "error": response["comment"]}
-            )
-        return HttpResponse(return_payload)
-    return HttpResponse({"error": "Method not allowed"})
+
+        try:
+            response = requests.get(
+                f"{constants.CODEFORCES_URL}api/user.info?handles={cf_handle}"
+            ).json()
+            if response["status"] != "OK":
+                raise Exception(response["comment"])
+
+            return_payload = {
+                "status": "OK",
+                "rating": response["result"][0]["rating"],
+                "profile_pic_url": response["result"][0]["titlePhoto"],
+            }
+        except Exception as ex:
+            return_payload = {
+                "status": "FAILED", "error": str(ex)
+            }
+        return Response(return_payload)
